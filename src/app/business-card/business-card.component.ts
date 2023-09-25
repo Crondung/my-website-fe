6e9e898b070ff6e07fb299e29615b24eecf42104
Crondung <1922635@stud.hs-mannheim.de>
2023-09-25 15:11:42 +0200

    dont flip card on icon click

--- src/app/business-card/business-card.component.ts
+++ src/app/business-card/business-card.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-business-card',
   templateUrl: './business-card.component.html',
-  styleUrls: ['./business-card.component.css']
+  styleUrls: ['./business-card.component.css'],
 })
 export class BusinessCardComponent {
+  isFlipped = false;
 
+  onCardClick(event: MouseEvent) {
+    let target: HTMLElement | null = event.target as HTMLElement;
+    while (target) {
+      if (
+        target.id === 'mail-icon' ||
+        target.id === 'github-icon' ||
+        target.id === 'linkedin-icon'
+      ) {
+        return;
+      }
+      target = target.parentElement;
+    }
+    this.isFlipped = !this.isFlipped;
+  }
 }
