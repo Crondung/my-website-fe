f815946b5006535407c9e30a5d075d103de42eec
Crondung <1922635@stud.hs-mannheim.de>
2023-09-26 00:25:04 +0200

    better structure, added hover effect

--- src/app/business-card/business-card.module.ts
+++ src/app/business-card/business-card.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnimatedLogoComponent } from '../animated-logo/animated-logo.component';
-import { BusinessCardBackComponent } from '../business-card-back/business-card-back.component';
-import { BusinessCardFrontComponent } from '../business-card-front/business-card-front.component';
 import { BusinessCardComponent } from './business-card.component';
 import { NgIconsModule } from '@ng-icons/core';
 import { iconoirGithub, iconoirLinkedin, iconoirMail } from '@ng-icons/iconoir';
+import { BusinessCardBackComponent } from './business-card-back/business-card-back.component';
+import { BusinessCardFrontComponent } from './business-card-front/business-card-front.component';
 
 @NgModule({
   declarations: [
